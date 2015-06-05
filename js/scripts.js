098c4b4eea2afcf04c37e1f5e42c9847af34c894
James Maynard <jameskirkmaynard@gmail.com>
2015-06-05 16:00:41 +0000

    Write jquery commands.

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,22 @@ var Number = {
   }
 };
 
+$(document).ready(function(event) {
+  event.preventDefault();
+  
+  var number = Object.create(Number);
+  var inputtedNumber = $("input#number").val();
+  var inputtedNumberMinusOne = number.numberMinusOne(inputtedNumber);
+  var zeros = number.numberZeros(inputtedNumber, inputtedNumberMinusOne);
+  var ones = number.numberOnes(zeros, inputtedNumber);
+  
+  number.number = inputtedNumber;
+  
+  $("div#results").append(
+    "<h3> And the results are in! </h3>" + "<br>" + "<h4> For 2 to the power of " number.number + "<h4>"
+   + "<br>" + "<h4> The amount of zeros is " + zeros + "<h4>" +
+     "<br>" + "<h4> The amount of ones is " + ones + "<h4>"
+  );
+  $("div#form").hide();
+  
+});
