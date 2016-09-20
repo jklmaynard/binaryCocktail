0428bbc1343766678190f6a935a6ba2725250552
James Maynard <jameskirkmaynard@gmail.com>
2016-09-20 14:07:02 -0700

    Add regex

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,18 @@ $(document).ready(function(event) {
     var inputtedNumber = $("input#number").val();
     var inputtedNumberMinusOne = equation.numberMinusOne(inputtedNumber);
     var zeros = equation.numberZeros(inputtedNumber, inputtedNumberMinusOne);
-    var ones = equation.numberOnes(zeros, inputtedNumber);
+    var zerosStringified = zeros.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    var ones = equation.numberOnes(zeros, inputtedNumber).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
     $("div#results").append(
-      "<h3> And the results are in! </h3>" +  
-      "<h4> For 2 to the power of " + inputtedNumber + " the cocktail is:</h4>" +  
-      "<h4 class='answer'>" + zeros + " parts zero, and " + ones + " parts one! </h4>").show();
+      "<h3> And the results are in! </h3>" +
+      "<h4> For 2 to the power of " + inputtedNumber + " the cocktail is:</h4>" +
+      "<h4 class='answer'>" + zerosStringified + " parts zero, and " + ones + " parts one! </h4>").show();
     $("div#explanation").hide();
     $("div#form").hide();
     $("button#return").show();
   });
-  
+
   $("button#return").click(function(event) {
     $("div#results").empty().hide();
     $("button#return").hide();
