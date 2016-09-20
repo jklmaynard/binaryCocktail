6eab9e1423064a04dabab5a67b341dd094728106
James Maynard <jameskirkmaynard@gmail.com>
2016-09-20 13:51:29 -0700

    Fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,14 @@ $(document).ready(function(event) {
     var equation = Object.create(Equation);
     var inputtedNumber = $("input#number").val();
     var inputtedNumberMinusOne = equation.numberMinusOne(inputtedNumber);
-    var zeros = equation.numberZeros(inputtedNumber, inputtedNumberMinusOne).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    var zeros = equation.numberZeros(inputtedNumber, inputtedNumberMinusOne);
+    var zerosStringified = zeros.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
     var ones = equation.numberOnes(zeros, inputtedNumber).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
     $("div#results").append(
       "<h3> And the results are in! </h3>" +
       "<h4> For 2 to the power of " + inputtedNumber + " the cocktail is:</h4>" +
-      "<h4 class='answer'>" + zeros + " parts zero, and " + ones + " parts one! </h4>").show();
+      "<h4 class='answer'>" + zerosStringified + " parts zero, and " + ones + " parts one! </h4>").show();
     $("div#explanation").hide();
     $("div#form").hide();
     $("button#return").show();
