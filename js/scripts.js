1bb38168cf1d24ce4e512a422fbe2a7707ea110d
James Maynard <jameskirkmaynard@gmail.com>
2016-09-20 13:42:01 -0700

    Add regex to get commas in the numbers

--- js/scripts.js
+++ js/scripts.js
@@ -19,18 +19,18 @@ $(document).ready(function(event) {
     var equation = Object.create(Equation);
     var inputtedNumber = $("input#number").val();
     var inputtedNumberMinusOne = equation.numberMinusOne(inputtedNumber);
-    var zeros = equation.numberZeros(inputtedNumber, inputtedNumberMinusOne);
-    var ones = equation.numberOnes(zeros, inputtedNumber);
+    var zeros = equation.numberZeros(inputtedNumber, inputtedNumberMinusOne).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    var ones = equation.numberOnes(zeros, inputtedNumber).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
     $("div#results").append(
-      "<h3> And the results are in! </h3>" +  
-      "<h4> For 2 to the power of " + inputtedNumber + " the cocktail is:</h4>" +  
+      "<h3> And the results are in! </h3>" +
+      "<h4> For 2 to the power of " + inputtedNumber + " the cocktail is:</h4>" +
       "<h4 class='answer'>" + zeros + " parts zero, and " + ones + " parts one! </h4>").show();
     $("div#explanation").hide();
     $("div#form").hide();
     $("button#return").show();
   });
-  
+
   $("button#return").click(function(event) {
     $("div#results").empty().hide();
     $("button#return").hide();
